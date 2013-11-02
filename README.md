9ee255bfd94effb321dd6c62d48a6a2374ebce4d
Rudger Gravestein <rudgergravestein@gmail.com>
2013-11-02 15:55:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# server cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Triple-networks (<r.gravestein@triple-networks.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'server'
+maintainer       'Triple-networks'
+maintainer_email 'r.gravestein@triple-networks.com'
+license          'Apache 2.0'
+description      'Installs/Configures server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+

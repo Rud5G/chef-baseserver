4c7e324a445f4cf9a243cfb777335d72c70e28b4
Rudger Gravestein <rudgergravestein@gmail.com>
2014-01-28 04:39:37 +0100

    Add failing tests

---
+++ test/integration/default/serverspec/ssh_daemon_spec.rb
@@ -0,0 +1,22 @@
+require 'serverspec'
+
+include Serverspec::Helper::Exec
+include Serverspec::Helper::DetectOS
+
+RSpec.configure do |c|
+  c.before :all do
+    c.path = '/sbin:/usr/sbin'
+  end
+end
+
+describe "SSH Daemon" do
+
+  it "is listening on port 22" do
+    expect(port(22)).to be_listening
+  end
+
+  it "has a running service of ssh-daemon" do
+    expect(service("sshd")).to be_running
+  end
+
+end

32f59d8fc1823185b3cf1fb886a0639a2d3eaa97
Rudger Gravestein <r.gravestein@drecomm.nl>
2014-03-14 21:27:45 +0100

    Update metadata dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license          'Apache 2.0'
 description      'Installs/Configures a base server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.4.1'
 
 %w( centos redhat ubuntu ).each do |os|
   supports os
 end
 
-%w( apt fail2ban git hostsfile ntp logrotate users vim yum ).each do |cb|
+%w( apt build-essential fail2ban git hostsfile ntp logrotate users vim yum ).each do |cb|
   depends cb
 end
 

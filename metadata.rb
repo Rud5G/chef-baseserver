6d2bc9ed27d4e026b025d743e633a8eb8d1ccc5a
Rudger <Rud5G@users.noreply.github.com>
2014-06-03 13:20:00 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license 'Apache 2.0'
 description 'Installs/Configures a base server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.6.2'
+version '0.6.3'
 
 %w( centos redhat ubuntu ).each do |os|
   supports os

6c241d34b24f462eaa7a16960c35d897ae5617a7
Rudger Gravestein <rudgergravestein@gmail.com>
2014-06-05 19:08:22 +0200

    Update metadata, bump version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,13 @@
-name 'baseserver'
-maintainer 'Triple-networks'
+name             'baseserver'
+maintainer       'Triple-networks'
 maintainer_email 'r.gravestein@triple-networks.com'
-license 'Apache 2.0'
-description 'Installs/Configures a base server'
+license          'Apache 2.0'
+description      'Installs/Configures a base server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.6.3'
+description      'Installs/Configures baseserver'
+long_description 'Installs/Configures baseserver'
+version          '0.6.4'
+
 
 %w( centos redhat ubuntu ).each do |os|
   supports os

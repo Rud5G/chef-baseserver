f8d6b1897bb3b6b55ab8b83e508b816cddc3bdd8
Rudger Gravestein <rudgergravestein@gmail.com>
2014-06-08 00:45:21 +0200

    Update dependencies, chefspec 4, fixed rubocop

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
 
-gem 'berkshelf', '~> 3.1.1'
-gem 'chefspec', '~> 3.3'
+gem 'berkshelf', '~> 3.1.3'
+gem 'chefspec', '~> 4.0'
 gem 'foodcritic', '~> 3.0.3'
-gem 'rubocop', '~> 0.21.0'
+gem 'rubocop', '~> 0.23.0'
 
 group :integration do
   gem 'test-kitchen', '~> 1.2'
--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name             'baseserver'
-maintainer       'Triple-networks'
+name 'baseserver'
+maintainer 'Triple-networks'
 maintainer_email 'r.gravestein@triple-networks.com'
-license          'Apache 2.0'
-description      'Installs/Configures a base server'
+license 'Apache 2.0'
+description 'Installs/Configures a base server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-description      'Installs/Configures baseserver'
+description 'Installs/Configures baseserver'
 long_description 'Installs/Configures baseserver'
-version          '0.7.1'
+version '0.7.1'
 
 %w( centos redhat ubuntu ).each do |os|
   supports os

50c0bed9a18628a6e70a8922f4f5f82b98550a7a
Rudger Gravestein <rudgergravestein@gmail.com>
2015-03-07 15:04:53 +0100

    Remove the double lines

--- metadata.rb
+++ metadata.rb
@@ -2,10 +2,8 @@ name 'baseserver'
 maintainer 'Triple-networks'
 maintainer_email 'r.gravestein@triple-networks.com'
 license 'Apache 2.0'
-description 'Installs/Configures a base server'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 description 'Installs/Configures baseserver'
-long_description 'Installs/Configures baseserver'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.7.11'
 
 # provide

725ea497ad8f6a1298dda1522b50310a38b28b9b
Rudger Gravestein <rudgergravestein@gmail.com>
2015-08-12 14:03:36 +0200

    Update dependencies + Vagrantfile log setting

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license 'Apache 2.0'
 description 'Installs/Configures baseserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.8.3'
+version '0.8.4'
 
 # provide
 provides 'baseserver::baseserver'
@@ -17,17 +17,15 @@ end
 depends 'apt', '~> 2.7.0'
 depends 'build-essential', '~> 2.2.3'
 depends 'fail2ban', '~> 2.2.1'
-depends 'git', '~> 4.2.2'
+depends 'git', '~> 4.3.3'
 depends 'hostsfile', '~> 2.4.5'
-depends 'logrotate', '~> 1.9.1'
-depends 'ntp', '~> 1.8.2'
+depends 'logrotate', '~> 1.9.2'
+depends 'ntp', '~> 1.8.6'
 depends 'users', '~> 1.8.2'
 depends 'vim'
-depends 'yum', '~> 3.6.0'
-
-# depends 'vim'
+depends 'yum', '~> 3.6.3'
 
 depends 'unattended-upgrades', '~> 0.1.2'
 
 # we moved to chef-zero, but still have an issue
-depends 'chef-solo-search', '~> 0.5.1'
+#depends 'chef-solo-search', '~> 0.5.1'

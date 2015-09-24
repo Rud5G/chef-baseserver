821889165efb6e3692d097efde432235e727ca05
Rudger Gravestein <rudgergravestein@gmail.com>
2015-09-24 18:07:42 +0200

    Update spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,25 +1,4 @@
-require 'simplecov'
-SimpleCov.start
-
 require 'chefspec'
-require 'coveralls'
-Coveralls.wear!
-
 require 'chefspec/berkshelf'
-require 'chef/application'
 
-def stub_resources
-end
-
-# user Matchers for chefspec
-if defined?(ChefSpec)
-  def create_users_manage(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:users_manage, :create, resource_name)
-  end
-
-  def remove_users_manage(resource_name)
-    ChefSpec::Matchers::ResourceMatcher.new(:users_manage, :remove, resource_name)
-  end
-end
-
-# at_exit { ChefSpec::Coverage.report! }
+at_exit { ChefSpec::Coverage.report! }

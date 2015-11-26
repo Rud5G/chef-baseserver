e7c95fff30bc2175ca41ed38a2fb08ddb54fe93f
Rudger Gravestein <rudgergravestein@gmail.com>
2015-11-26 16:13:56 +0100

    Fix right version: 0.8.6
    (should be minor)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license 'Apache 2.0'
 description 'Installs/Configures baseserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.9.0'
+version '0.8.6'
 
 # provide
 provides 'baseserver::baseserver'

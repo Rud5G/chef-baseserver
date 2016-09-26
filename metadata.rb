8e48a6110193002f045711a7c574000642621e64
Rudger Gravestein <rudgergravestein@gmail.com>
2016-09-26 13:51:01 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'r.gravestein@triple-networks.com'
 license 'Apache 2.0'
 description 'Installs/Configures baseserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-#issues_url 'https://github.com/Rud5G/chef-baseserver/issues' if respond_to?(:issues_url)
-#source_url 'https://github.com/Rud5G/chef-baseserver' if respond_to?(:source_url)
+issues_url 'https://github.com/Rud5G/chef-baseserver/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/Rud5G/chef-baseserver' if respond_to?(:source_url)
 version '0.9.2'
 
 # provide

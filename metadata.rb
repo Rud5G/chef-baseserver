975805b510970c2c0bc791147b20a0a7146a0aec
Rudger <Rud5G@users.noreply.github.com>
2017-08-23 19:17:54 +0200

    Update packages (#82)

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures baseserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/Rud5G/chef-baseserver/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/Rud5G/chef-baseserver' if respond_to?(:source_url)
-version '0.9.3'
+version '0.9.4'
 
 # provide
 provides 'baseserver::baseserver'
@@ -14,14 +14,14 @@ provides 'baseserver::baseserver'
 # support
 supports 'ubuntu'
 
-depends 'apt', '~> 6.1.0'
-depends 'build-essential', '~> 8.0.1'
-depends 'fail2ban', '~> 4.0.0'
-depends 'git', '~> 6.0.0'
-depends 'hostsfile', '~> 2.4.5'
+depends 'apt', '~> 6.1.3'
+depends 'build-essential', '~> 8.0.3'
+depends 'fail2ban', '~> 4.0.1'
+depends 'git', '~> 6.1.0'
+depends 'hostsfile', '~> 3.0.1'
 depends 'locales', '~> 0.3.1'
-depends 'logrotate', '~> 2.1.0'
+depends 'logrotate', '~> 2.2.0'
 depends 'logwatch', '~> 2.1.1'
-depends 'ntp', '~> 3.3.1'
-depends 'users', '~> 4.0.3'
+depends 'ntp', '~> 3.5.1'
+depends 'users', '~> 5.1.0'
 depends 'vim', '~> 2.0.2'
